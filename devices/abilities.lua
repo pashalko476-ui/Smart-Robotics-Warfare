f9e25488aca1d32fa889e371993072df33e1eef5
МеГа КеГа <pashalko476@gmail.com>
2026-07-10 17:07:34 +0300

    Update abilities.lua

--- devices/abilities.lua
+++ devices/abilities.lua
@@ -1,1 +1,33 @@
+-- effects.lua
+-- Визуальные эффекты для всех типов оружия и снарядов
+
+local Effects = {
+
+    ---------------------------------------------------------
+    -- СЛЕДЫ СНАРЯДОВ
+    ---------------------------------------------------------
+
+    bullet_trail = {
+        type = "trail",
+        color = {1.0, 0.9, 0.6},
+        width = 2,
+        duration = 0.15,
+    },
+
+    shell_trail = {
+        type = "trail",
+        color = {1.0, 0.8, 0.4},
+        width = 4,
+        duration = 0.25,
+    },
+
+    heavy_shell_trail = {
+        type = "trail",
+        color = {1.0, 0.7, 0.3},
+        width = 6,
+        duration = 0.35,
+    },
+
+    pulse_trail = {
+        type = "trail",
 
