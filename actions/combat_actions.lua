d7749dad5a524530c3bf5e05f9462e9a80e4d3d2
МеГа КеГа <pashalko476@gmail.com>
2026-07-10 17:56:57 +0300

    Update combat_actions.lua

--- actions/combat_actions.lua
+++ actions/combat_actions.lua
@@ -1,1 +1,10 @@
+local Weapons = require(path .. "/devices/weapons")
+function CombatActions.OnProjectileHit(robot, weaponName, projectileName, target)
+    local result = Weapons.ApplyDamage(weaponName, projectileName, target)
+
+    if result then
+        target:ApplyDamage(result.damage)
+        target:ApplyImpulse(result.impulse)
+    end
+end
 
