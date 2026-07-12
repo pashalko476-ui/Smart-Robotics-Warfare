35a302050256df695a2b1219c9897ee81a953a5c
МеГа КеГа <pashalko476@gmail.com>
2026-07-12 20:13:02 +0300

    Update scout.lua

--- ai/tier1/scout.lua
+++ ai/tier1/scout.lua
@@ -2,6 +2,7 @@
 
 local BaseAI = require(path .. "/ai/ai_base.lua")
 local ScoutAI = setmetatable({}, { __index = BaseAI })
+robot.missiles = {}
 
 function ScoutAI:Init(id)
     BaseAI.Init(self, id)
