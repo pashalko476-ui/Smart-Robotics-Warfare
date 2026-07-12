bc366cdb325a2a0a6338ffb6436c6556235c2416
МеГа КеГа <pashalko476@gmail.com>
2026-07-12 20:12:34 +0300

    Update gunner.lua

--- ai/tier1/gunner.lua
+++ ai/tier1/gunner.lua
@@ -2,6 +2,7 @@
 
 local BaseAI = require(path .. "/ai/ai_base.lua")
 local GunnerAI = setmetatable({}, { __index = BaseAI })
+robot.missiles = {}
 
 function GunnerAI:Init(id)
     BaseAI.Init(self, id)
